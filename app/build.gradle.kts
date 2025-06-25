a972c399b8694df79b947b0d7fc9b52e95b29cab
VictorGotsenko <victor.gotsenko@gmail.com>
2025-06-25 19:54:09 +0500

    Add Dockerfile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,14 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     id("java")
     id("application")
     id("checkstyle")
     id("jacoco")
+
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
     id("com.github.ben-manes.versions") version "0.51.0"
 
@@ -21,6 +27,20 @@ repositories {
 }
 
 dependencies {
+    // Javalin
+    implementation("io.javalin:javalin:6.5.0")
+    implementation("io.javalin:javalin-rendering:6.5.0")
+    implementation("org.slf4j:slf4j-simple:2.0.16")
+    implementation("gg.jte:jte:3.1.16")
+
+
+    // LOMBOK
+    compileOnly("org.projectlombok:lombok:1.18.38")
+	annotationProcessor("org.projectlombok:lombok:1.18.38")
+
+	testCompileOnly("org.projectlombok:lombok:1.18.38")
+	testAnnotationProcessor("org.projectlombok:lombok:1.18.38")
+
 
 
     // CheckStyle
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,26 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static Javalin getApp() { //throws SQLException
+
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+
+        app.get("/", ctx ->{ctx.result("Hello World");});
+        return app;
+    }
+
+    public static void main(String[] args) { //throws SQLException
+        Javalin app = getApp();
+        app.start(7070);
     }
 }
